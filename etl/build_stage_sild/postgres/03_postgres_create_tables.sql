e18a7802fd6f13479e05ecca1a8050b8dc7fc86c
theonesp <ma.armengol.hcsc@gmail.com>
2022-04-06 13:46:40 +0200

    Update 03_postgres_create_tables.sql

--- etl/build_stage_sild/postgres/03_postgres_create_tables.sql
+++ etl/build_stage_sild/postgres/03_postgres_create_tables.sql
@@ -36,3 +36,43 @@ CREATE TABLE  r_monitor(
   ,VALORS           VARCHAR
   ,AILLAT           VARCHAR
 );
+
+
+--------------------------------------------------------
+--  DDL for Table r_monitor
+--------------------------------------------------------
+
+-- Drop table
+-- DROP TABLE stage_sild.r_monitor_parsed source
+
+-- This view extracts the value for each vital sign in the table r_monitor using regexp.
+CREATE OR REPLACE VIEW stage_sild.r_monitor_parsed
+AS SELECT r_monitor.mandt,
+    r_monitor.id_score,
+    r_monitor.data,
+    r_monitor.hora,
+    r_monitor.patnr,
+    r_monitor.id,
+    r_monitor.usuari,
+    r_monitor.vpid,
+    r_monitor.vwert,
+    r_monitor.pop_up,
+    r_monitor.envio_medxat_med,
+    r_monitor.envio_medxat_enf,
+    r_monitor.envio_email, 
+    "substring"(r_monitor.valors::text, 'ESTADO_CONCIENC_(.)'::text) AS conc_state,
+    "substring"(r_monitor.valors::text, 'FC_EKG=([[:digit:]]*)'::text) AS hr_ecg,
+    "substring"(r_monitor.valors::text, 'FC_OSC=([[:digit:]]*)'::text) AS hr_osc,
+    "substring"(r_monitor.valors::text, 'FR_IP=([[:digit:]]*)'::text) AS rr_ip,
+    "substring"(r_monitor.valors::text, 'FREC_RESP=([[:digit:]]*)'::text) AS rr,     
+    "substring"(r_monitor.valors::text, 'O2_DIS_(.)'::text) AS o2_sup,    
+    "substring"(r_monitor.valors::text, 'PA_S=([[:digit:]]*)'::text) AS pa_s,      
+    "substring"(r_monitor.valors::text, 'PRESN_SIS=([[:digit:]]*)'::text) AS presn,
+    "substring"(r_monitor.valors::text, 'PULSIOX=([[:digit:]]*)'::text) AS pulsiox,   
+    "substring"(r_monitor.valors::text, 'PULSO=([[:digit:]]*)'::text) AS pulse,
+    "substring"(r_monitor.valors::text, 'TEMP_AXI=([[:digit:]]*\.[[:digit:]])'::text) AS temp_axi,
+    -- "substring"(r_monitor.valors::text, 'TEMP_BU=([[:digit:]]*\.[[:digit:]])'::text) AS temp_bu,    
+    -- "substring"(r_monitor.valors::text, 'TEMP_CT=([[:digit:]]*\.[[:digit:]])'::text) AS temp_ct,       
+    -- "substring"(r_monitor.valors::text, 'TEMP_TIM=([[:digit:]]*\.[[:digit:]])'::text) AS temp_tim,    
+    r_monitor.aillat
+   FROM stage_sild.r_monitor;
