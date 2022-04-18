9ade69852b61dd9f8bec1b7b41a0e429588652c2
theonesp <ma.armengol.hcsc@gmail.com>
2022-04-18 21:40:15 +0200

    Update and rename r_monitors_p_long.sql to chartevents.sql
    chartevents view code is ordered and all available items (11) are converted from wide to long

---
+++ build_p_zero/etl/postgres/chartevents/chartevents.sql
@@ -0,0 +1,169 @@
+-- This view converts r_monitor_parsed data from wide to long format
+-- The index is generated ordering the items alphabetically, according to the order they were received.
+-- The first table that was received was r_monitor
+
+--------------------------------------------------------
+--  DDL for Table chartevents
+--------------------------------------------------------
+
+-- Drop table
+-- DROP TABLE p_zero.chartevents
+
+CREATE OR REPLACE VIEW p_zero.chartevents
+AS 
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	1 AS item_id,
+	conc_state AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	conc_state IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	2 AS item_id,
+	hr_ecg AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	hr_ecg IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	3 AS item_id,
+	hr_osc AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	hr_osc IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	4 AS item_id,
+	rr_ip AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	rr_ip IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	5 AS item_id,
+	rr AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	rr IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	6 AS item_id,
+	o2_sup AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	o2_sup IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	7 AS item_id,
+	pa_s AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	pa_s IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	8 AS item_id,
+	presn AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	presn IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	9 AS item_id,
+	pulsiox AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	pulsiox IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	10 AS item_id,
+	pulse AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	pulse IS NOT NULL
+UNION ALL
+
+SELECT
+	patnr,
+	usuari,
+	vpid,
+	DATA AS store_date,
+	hora AS store_time,
+	11 AS item_id,
+	temp_axi AS value
+FROM
+	stage_sild.r_monitor_parsed
+WHERE
+	temp_axi IS NOT NULL
+ORDER BY
+	patnr,
+	store_date,
+	store_time
