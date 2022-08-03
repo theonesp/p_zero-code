38d539b13b684b0abf1ef3eb830eb5d4d8065d26
Miguel Á. Armengol de la Hoz <ma.armengol.hcsc@gmail.com>
2022-08-03 10:48:06 +0200

    Update 04_postgres_ingest_data.sql

--- build_p_zero_stage/postgres/04_postgres_ingest_data.sql
+++ build_p_zero_stage/postgres/04_postgres_ingest_data.sql
@@ -3,4 +3,6 @@
 SET SEARCH_PATH TO data_scope;
 
 --copying demog
-\copy data_scope.demog (patient_ref, birth_date, sex, nation_ref) FROM '/stage_files/demo_2022-07-28.txt' DELIMITER ';' CSV HEADER;
+\copy data_scope.demog (patient_ref, birth_date, sex, nation_ref,load_date) FROM '/stage_files/demo_2022-07-28.txt' DELIMITER ';' CSV HEADER;
+
+
