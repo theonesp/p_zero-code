33e55c4adf9eb7b4685ba6bcfc9d09ac11022710
Miguel Á. Armengol de la Hoz <ma.armengol.hcsc@gmail.com>
2022-08-03 16:55:16 +0200

    Update 04_postgres_ingest_data.sql
    all files so far copy code added

--- build/01_data_scope/postgres/04_postgres_ingest_data.sql
+++ build/01_data_scope/postgres/04_postgres_ingest_data.sql
@@ -2,7 +2,11 @@
 
 SET SEARCH_PATH TO data_scope;
 
+--copying natio_dic.csv
+\copy data_scope.nation_dic (natio_ref, "label", tt) FROM '/stage_files/natio_dic.csv' DELIMITER ';' CSV HEADER;
+
 --copying demog
 \copy data_scope.demog (patient_ref, birth_date, sex, nation_ref,load_date) FROM '/stage_files/demo_2022-07-28.txt' DELIMITER ';' CSV HEADER;
 
-
+--copying znt_score
+\copy data_scope.znt_score (patient_id, id, mandt, id_score, "date", hora, usuari, vpid, vwert, pop_up, envio_medxat_med, envio_medxat_enf, envio_email, valors, aillat, ou_med_ref, care_level_ref) FROM '/stage_files/znt_score_2022-07-28.txt' DELIMITER ';' CSV HEADER;
