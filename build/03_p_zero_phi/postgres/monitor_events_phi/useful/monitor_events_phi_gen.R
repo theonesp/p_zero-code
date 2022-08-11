8ec9cce24452fc3a3c5f41b882e2677072b84f93
xborrat <xborrat@users.noreply.github.com>
2022-08-11 11:03:33 -0400

    Update monitor_events_phi_gen.R
    Added a "," after care_level_ref
    Remove print() which created a NULL as it was combined with cat.

--- build/03_p_zero_phi/postgres/monitor_events_phi/useful/monitor_events_phi_gen.R
+++ build/03_p_zero_phi/postgres/monitor_events_phi/useful/monitor_events_phi_gen.R
@@ -4,13 +4,12 @@
 #('ESTADO_CONCIENC_','FC_EKG','FC_OSC','FR_IP','FREC_RESP','O_DIS_','PA_S','PRESN_SIS','PULSIOX','PULSO','TEMP_AXI')
 vitals<-c('conc_state','hr_ecg','hr_osc','rr_ip','rr','o2_sup','pa_s','presn','pulsiox','pulse','temp_axi')
 for (i in 1:length(vitals)) {
-  print(
     cat(
       'SELECT
 patient_ref,
 phi_id,
 care_level_ref,
-ou_med_ref
+ou_med_ref,
 monitor_events_user,
 result_date,
 score,
@@ -18,10 +17,9 @@ group_flag,
 ',i,'AS reg_clin_ref,   
 ',vitals[i],'AS value
 FROM
-stage_sild.r_monitor_parsed
+p_zero_stage.znt_score_tp
 WHERE',vitals[i],'IS NOT NULL
 UNION ALL
-      '
-    )
-  )
+      ')
+
 }
