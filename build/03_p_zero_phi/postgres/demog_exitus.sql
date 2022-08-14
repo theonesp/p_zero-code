26dfd4672b0806a0ac6b3040c75349c459343064
xborrat <xborrat@users.noreply.github.com>
2022-08-13 21:59:24 -0400

    Create demog_exitus.sql
    first commit

---
+++ build/03_p_zero_phi/postgres/demog_exitus.sql
@@ -0,0 +1,10 @@
+-- Fusion of demog table with exitus data in order to provide prognostic insights.
+
+CREATE TABLE p_zero_phi.demog AS
+SELECT 
+  data_scope.demog.*, date(p_zero_stage.exitus_events.exitus_date) as exitus_date
+FROM
+  data_scope.demog 
+  JOIN
+  p_zero_stage.exitus_events
+  USING (patient_ref)
