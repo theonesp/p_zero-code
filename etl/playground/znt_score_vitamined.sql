714ba8baa7628e0aee384c706753a0dedce1208d
xborrat <xborrat@yahoo.es>
2022-09-28 16:53:04 -0400

    full sql to obtain znt_scope stay_scope

--- etl/playground/znt_score_vitamined.sql
+++ etl/playground/znt_score_vitamined.sql
@@ -1,5 +1,8 @@
 -- requires previous tables: znt_score, stay.
 
+
+-- creation of znt_score_phi  which is znt_score with nmr(patnr).
+
 DROP TABLE IF EXISTS playground.znt_score_phi;
 
 CREATE TABLE playground.znt_score_phi (
@@ -9,7 +12,7 @@ CREATE TABLE playground.znt_score_phi (
 	id_score varchar NULL,
 	"data" date NULL,
 	hora varchar NULL,
-    	patnr int4 NULL,
+    patnr int4 NULL,
 	usuari varchar NULL,
 	vpid int4 NULL,
 	vwert int4 NULL,
@@ -27,7 +30,7 @@ CREATE TABLE playground.znt_score_phi (
 copy playground.znt_score_phi  FROM '/home/admin/xavi_cabin/znt_score_phi.csv' DELIMITER ',' CSV HEADER;
 
 
---CREATE NEW TABLE WITH REF TO PHI
+--CREATE NEW  znt TABLE WITH deidentified unique patient code (paient_ref) and  PHI
 --Join the new znt_score with patnr(phi) with previous  znt_score with patient_id(no phi)
 
 drop table if exists playground.znt_ref_phi;
@@ -43,11 +46,12 @@ p.data=z.data and p.hora=z.hora and p.valors=z.valors
 --CREATE TABLE playground.znt_ref_phi_filled
 -- to solve the problem that there’s nulls in patient_id 
 
--- OBJECTIVES: create new table with the gaps in patient_id columns filled. 
+-- OBJECTIVES: create a new table with the gaps in patient_id columns filled. 
 -- creates a table key to transform patnr to patient_id (creation and selecting 
 -- distinct rows with groupby)
--- using the key with the original table znt_score
+-- using the key with the original table znt_score.
 
+Drop table if exists playground.znt_ref_phi_filled;
 CREATE TABLE playground.znt_ref_phi_filled AS
 (
 WITH output1 as(
@@ -60,6 +64,7 @@ order by o.patient_id,data, hora)
 select patient_ref,id,mandt,id_score, data, hora, patnr, usuari, vpid, vwert,envio_medxat_med,envio_medxat_enf,envio_email,valors,aillat from output2)
 
 
+
 DROP TABLE IF EXISTS playground.moviments;
 
 CREATE TABLE playground.moviments (
@@ -70,7 +75,7 @@ CREATE TABLE playground.moviments (
 
 );
 
--- INGEST MOVIMENTS DATA
+-- INGEST MOVIMENTS DATA: 
 
 copy playground.moviments FROM  '/home/admin/xavi_cabin/mov_2022-09-24.txt' DELIMITER ';' CSV HEADER;
 
@@ -83,6 +88,7 @@ In the same script I merge left join znt score and movements.
 
 
 DROP TABLE IF EXISTS playground.znt_phi_loc;
+
 CREATE TABLE playground.znt_phi_loc AS(
 with znt_rank0 as --len of valors column
     (SELECT *,char_length(valors) as len FROM playground.znt_ref_phi_filled),
@@ -117,14 +123,26 @@ select patient_ref,
 );
 
 
--- Colapsar files a dies per poder seleccionar dies anteriors a ingres a uci,intemitjos,paro.
--- Utilitzarem dia d'ingrés per separar els diferents episodis.
+-- Merge stay and znt_phi_loc to obtain the date of hospital admission and thus episode(hosp_adm).
+
+DROP TABLE IF EXISTS playgroud.znt_phi_loc_stay;
+
+CREATE TABLE  playground.znt_phi_loc_stay AS
+(select z.*,s.start_date as ingres,s.end_date as alta from playground.znt_phi_loc z left join playground.stay s 
+on z.patient_ref = s.patient_ref and z.data=s.data)
+
+
+
+
+
+-- Collapse rows to days to be able to select the days prior to admission to uci, intermediate , shock room.
+-- Use the day of entry to separate the different episodes.
 
 DROP TABLE IF EXISTS collapsed_znt;
 
 CREATE TABLE collapsed_znt AS
 (
-with output1 as -- agrupacio per pacient, sala , dia.
+with output1 as -- aggregate by pacient, ward, day
 (SELECT 
     patient_ref,
     max(patnr)as patnr,
@@ -137,15 +155,10 @@ FROM playground.znt_phi_loc_stay group by patient_ref,ou_loc_ref,data
 having  count(*) >1 and max(ou_loc_ref) is NOT NULL
 order by patient_ref,ou_loc_ref,data
 ),
-output2 as -- funcions finestra on cada finestra equival a un episodi(pacient,ingrés)
+output2 as -- windows functions where each window  is related to  an hospital_adm (pacient,ingres)
 (select *,
     case
-        when ou_loc_ref like 'G%' and 
-            (lead(ou_loc_ref,7) over (partition by patient_ref,ingres order by patient_ref,data) similar to '(E|I|BPARO|PUR0)%')
-            THEN 1
-        when ou_loc_ref like 'G%' and 
-            (lead(ou_loc_ref,6) over (partition by patient_ref,ingres order by patient_ref,data) similar to '(E|I|BPARO|PUR0)%')
-            THEN 1
+ 
         when ou_loc_ref like 'G%' and 
             (lead(ou_loc_ref,5) over (partition by patient_ref,ingres order by patient_ref,data) similar to '(E|I|BPARO|PUR0)%')
             THEN 1
@@ -163,11 +176,152 @@ output2 as -- funcions finestra on cada finestra equival a un episodi(pacient,in
             THEN 1
         when (ou_loc_ref similar to '(E|I|BPARO|PUR0)%' ) and 
             LAG(ou_loc_ref) over (partition by patient_ref,ingres order by patient_ref,data) like 'G%' 
-            THEN 2 -- 
-        when ou_loc_ref like 'G%' --resta de pacients que estan en una sala G i no compleixen cases anterior.
+            THEN 2 -- 2) intensive care/intermdiate/PUR0
+        when ou_loc_ref like 'G%' -- 3)resta de pacients que estan en una sala G i no compleixen cases anterior.
             THEN 3
-        else 0 --inclou altres sales que no son (G,E,I,BPARO,PUR0)
+        else 0 --inclou altres sales que no son (G,E,I,BPARO,PUR0 etc)
         END as intensive_care_level
 from output1 
 )
-select * from output2 where  intensive_care_level::VARCHAR ~ '(1|2|3)'); --FILTRAT PER 
+select * from output2 where  intensive_care_level::VARCHAR ~ '(1|3)' --1) normal ward no need intensive care 2) intensive care
+-- 3) normal ward ending in the intensive care.
+); 
+
+
+
+-- Stay_frame creation:
+-- Collapse to obtain  stay_frame gralunarity.
+-- Stay_frame concept: periods of normal ward stays that ends or not with the need of critical care admission 
+-- (1: need for intensive admision , 3: no need for intensive admision )
+
+drop table if exists playground.stay_frame;
+create table playground.stay_frame as
+
+(with output1 as(
+select patient_ref, ingres, loc_adm_date ,ou_loc_ref,min(intensive_care_level) from playground.collapsed_znt 
+group by patient_ref, ingres,loc_adm_date, ou_loc_ref order by patient_ref, loc_adm_date,ingres
+),
+output2 as (
+select *,  row_number() over (partition by patient_ref,ingres,loc_adm_date order by patient_ref, loc_adm_date,ingres )
+from output1) as stay_frame_seq
+select * from output2 
+order by patient_ref, ingres, loc_adm_date)
+
+
+
+-- hosp_adm creation:
+-- Collapse to obtain hosp_adm gralunarity.
+drop table if exists playground.hosp_adm;
+Create table playground.hosp_adm as
+(with output1 as(
+select patient_ref, ingres as hosp_adm from playground.collapsed_znt 
+group by patient_ref, ingres order by patient_ref, ingres
+),
+output2 as (
+select *,  row_number() over (partition by patient_ref order by patient_ref, hosp_adm ) as hosp_adm_seq
+from output1)
+select * from output2 
+order by patient_ref, hosp_adm )
+
+
+
+-- place unique identifiers to hosp_adm and stay_frame
+ALTER TABLE playground.stay_frame ADD COLUMN stay_frame_id SERIAL PRIMARY KEY;
+ALTER TABLE playground.hosp_adm ADD COLUMN hosp_adm_id SERIAL PRIMARY KEY;
+
+
+
+
+-- CREATION znt_master
+-- merge   hosp_adm, stay_id wiht  znt_loc_stay, not repeated columns selection, name change, filter the vitals rows that has 
+-- no stay_frame_id
+
+DROP TABLE IF EXISTS playground.znt_master;
+CREATE TABLE playground.znt_master as
+(
+SELECT 
+    z.patient_ref,
+    z.ingres as hosp_adm_date,
+    alta as hosp_discharge_date,
+    hosp_adm_seq,
+    hosp_adm_id,
+    stay_frame_id,
+    stay_frame_seq,
+    min as evolution,
+  --
+    id,
+    mandt,
+    id_score,
+    data,
+    hora,
+    patnr,
+    z.ou_loc_ref,
+    start_date as loc_adm_date,
+    usuari,
+    vpid,
+    vwert,
+    z.envio_medxat_med,
+    envio_medxat_enf,
+    valors,
+    aillat
+  
+
+FROM playground.znt_phi_loc_stay z left join playground.hosp_adm adm 
+on z.patient_ref=adm.patient_ref and z.ingres=adm.hosp_adm left join playground.stay_frame sf  
+on z.patient_ref=sf.patient_ref and z.ingres=sf.ingres and z.start_date=sf.loc_adm_date
+where stay_frame_id IS NOT NULL -- filter for all rows that have stay_frame
+);
+
+
+
+
+
+-- creació stay_scope : colapsa znt_loc_master a nivell de stay frames i borrar columnes de dades.
+
+DROP TABLE IF EXISTS playground.stay_scope;
+CREATE TABLE playground.stay_scope as
+(
+SELECT 
+    patient_ref,
+    hosp_adm_id,
+    max(hosp_adm_date) as hosp_adm_date,
+    max(hosp_discharge_date) as hosp_discharge_date,
+    max(hosp_adm_seq) as hosp_adm_seq ,
+    stay_frame_id,
+    max(stay_frame_seq) as stay_frame_seq,
+    max(evolution) as evolution
+FROM playground.znt_master
+GROUP BY patient_ref,hosp_adm_id,stay_frame_id
+);
+
+
+-- creació znt_scope: elimina columnes cd znt_master i només deixar dades i claus de conexio amb stay_scope
+
+DROP TABLE IF EXISTS playground.znt_scope;
+CREATE TABLE playground.stay_scope as
+(
+SELECT 
+    patient_ref,
+    hosp_adm_id,
+    stay_frame_id,
+  --
+    id,
+    mandt,
+    id_score,
+    data,
+    hora,
+    patnr,
+    ou_loc_ref,
+    loc_adm_date,
+    usuari,
+    vpid,
+    vwert,
+    envio_medxat_med,
+    envio_medxat_enf,
+    valors,
+    aillat
+FROM playground.znt_master
+);
+
+
+
